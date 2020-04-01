57d7e2b4f8bf0e3640f39837908bcaebf5c43f11
DFrank <damon.frankfurth@gmail.com>
2020-04-01 11:37:09 -0600

    Added styling to login.

--- application/views/includes/styles.php
+++ application/views/includes/styles.php
@@ -1,4 +1,1 @@
 <link rel="stylesheet" href="<?php echo base_url(); ?>static/css/main.css" type="text/css" media="screen" />
-<link rel="stylesheet" href="<?php echo base_url(); ?>static/css/mob.css" type="text/css" media="screen and (max-width: 800px)" />
-<link rel="stylesheet" href="<?php echo base_url(); ?>static/css/mob_w.css" type="text/css" media="screen and (max-width: 1366px)" />
-<link href="https://fonts.googleapis.com/css?family=Share+Tech+Mono" rel="stylesheet" />
--- application/views/login.php
+++ application/views/login.php
@@ -1,14 +1,14 @@
 <?php $this->load->view("includes/header.php"); ?>
 <body>
-	<div class="page">
-		<div class="sw_back loginBlock">
-			<san><?php echo $error; ?></span>
-			<?php
-			echo validation_errors('<span>', '</span>');
-			$attrs = array('id' => 'loginForm', 'name' => 'loginForm');
-			echo form_open('login', $attrs); ?>
-				<p><label for="user_name">Username:</label><input type="text" name="user_name" ng-model="logCont.login.user_name" autofocus required/></p>
-				<p><label for="password">Password:</label><input type="password" name="password" ng-model="logCont.login.password" required/></p>
+	<div class="sw_back page">
+		<div class="sw_back_s login">
+			<span><?php echo $error; ?></span>
+				<?php
+				echo validation_errors('<span>', '</span>');
+				$attrs = array('id' => 'loginForm', 'name' => 'loginForm');
+				echo form_open('login', $attrs); ?>
+				<p><label for="user_name">Username:</label><input type="text" name="user_name" autofocus required/></p>
+				<p><label for="password">Password:</label><input type="password" name="password" required/></p>
 				<button type="submit" name="submit">Login</button>
 			</form>
 		</div>
